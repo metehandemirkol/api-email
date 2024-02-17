dd42956c243df3d96344e6ebe6bef13c40888df1
metehandemirkol <demirkolmetehan@hotmail.com>
2024-02-17 15:56:05 -0800

    Add links,specify language,add subject

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
-
+topic = "tesla"
 api_key = "c15eb79e866d435689bcfcd228807e91"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
       "from=2024-01-13&sortBy=publishedAt&apiKey=" \
-      "c15eb79e866d435689bcfcd228807e91"
+      "c15eb79e866d435689bcfcd228807e91&"
+      "language=en"
 #Make request
 request = requests.get(url)
 
@@ -12,7 +14,13 @@ content = request.json()
 
 #Access the article titles and description
 
-for article in content["articles"]:
-      print(article["title"])
-      print(article["description"])
-      
+body = ''
+for article in content["articles"][:20]:
+      if article["title"]   is not None:
+            body = body + article["title"] + "\n"\
+                   + article['description']\
+                   +"\n" +article["url"] + 2*"\n"
+
+
+body = body.encode("utf-8")
+send_email(message=body)
